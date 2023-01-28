0dc86a1c9c56fc035e27737eb4fc48c0865f3c6b
ukrbride4u <ukrbride4u@gmail.com>
2023-01-28 15:42:43 +0200

    first breath

---
+++ server.js
@@ -0,0 +1,17 @@
+const app = require("./app");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const { DB_HOST, PORT } = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connected successfuly");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
