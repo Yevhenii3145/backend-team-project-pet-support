1a5ef8747e06331b3f418d354217527e95e818f4
MaksymDziubanchuk <94477065+MaksymDziubanchuk@users.noreply.github.com>
2023-01-30 22:50:01 +0200

    base structure fixed

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const app = require("./app");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const { DB_HOST, PORT } = process.env;
+const app = require('./app')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const { DB_HOST, PORT = 4001 } = process.env
 
-mongoose.set("strictQuery", true);
+mongoose.set('strictQuery', true)
 
 mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(PORT);
-    console.log("Database connected successfuly");
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
+    .connect(DB_HOST)
+    .then(() =>
+        app.listen(PORT, () => {
+            console.log(`Database connected successfuly on port: ${PORT}`.bold)
+        })
+    )
+    .catch((error) => {
+        console.log(error.message)
+        process.exit(1)
+    })
