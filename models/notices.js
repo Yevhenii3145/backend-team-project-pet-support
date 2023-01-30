d716eca1f4086f6e14a57c3a97447f3d1b98fe6e
Serhii Kudriavtsev <96264045+SerhiiKudriavtsev@users.noreply.github.com>
2023-01-30 21:49:20 +0200

    Creat model notices

---
+++ models/notices.js
@@ -0,0 +1,76 @@
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
+
+const { handleMongooseError } = require('../helpers');
+
+const noticeSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Type a title'],
+    },
+    petName: {
+      type: String,
+      required: [true, "Type the pet's name"],
+    },
+    petDateOfBirth: {
+      type: Date,
+      required: true,
+    },
+    petBreed: {
+      type: String,
+      default: '',
+    },
+    petSex: {
+      type: String,
+      enum: ['male', 'female'],
+      default: 'male',
+    },
+    petLocation: {
+      type: String,
+      required: true,
+    },
+    petPrice: {
+      type: Number,
+      default: 0,
+    },
+    petImage: {
+      type: String,
+      required: false,
+    },
+    petComments: {
+      type: String,
+      default: '',
+    },
+    petCategory: {
+      type: String,
+      enum: ['lost-found', 'in-good-hands', 'sell'],
+      required: true,
+    },
+    petOwner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      // required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+noticeSchema.post('save', handleMongooseError);
+const Notice = model('notice', noticeSchema);
+
+const addSchema = Joi.object({
+  title: Joi.string().required(),
+  petName: Joi.string().required(),
+  petDateOfBirth: Joi.string().required(),
+  petSex: Joi.string().allow('male', 'female'),
+  petLocation: Joi.string().required(),
+  petPrice: Joi.number(),
+  petBreed: Joi.string(),
+  petComments: Joi.string(),
+  petCategory: Joi.string().allow('lost-found', 'for-free', 'sell').required(),
+});
+
+const schemas = { addSchema };
+
+module.exports = { Notice, schemas };
