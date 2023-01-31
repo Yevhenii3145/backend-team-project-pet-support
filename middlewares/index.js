24c7c85ae78ea5deeb072ce430ed3bd752fd64be
ukrbride4u <ukrbride4u@gmail.com>
2023-01-31 15:04:47 +0200

    isValidId

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const handleMongooseError = require('./handleMongooseError')
 const upload = require('./upload')
 const authentification = require('./authentification')
 const validateBody = require('./validateBody')
+const isValidId = require('./isValidId')
 
 module.exports = {
     handleMongooseError,
     upload,
     authentification,
     validateBody,
+    isValidId,
 }
