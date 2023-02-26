e889ddfc69b8dd170f2ac6efeb92ff3f1ce50776
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2023-02-27 00:12:48 +0200

    Update authentification.js

--- controllers/pets/authentification.js
+++ controllers/pets/authentification.js
@@ -7,7 +7,7 @@ const authentification = async (req, res, next) => {
     const { authorization = '' } = req.headers
     const [bearer, token] = authorization.split(' ')
     if (bearer !== 'Bearer' || !token) {
-        nnext(HttpError(400, 'Token not found'))
+        next(HttpError(400, 'Token not found'))
     }
     try {
         const { id } = jwt.verify(token, SECRET_KEY)
